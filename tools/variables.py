e81f16b87ec4d6d73564ced691a279628ed517ce
Gary Menezes <garymenezes@gary-imac.lan>
2022-01-04 20:57:35 -0800

    adding default start date to global variables

--- tools/variables.py
+++ tools/variables.py
@@ -7,3 +7,7 @@ MONTH_DICT_REV = {v:k for k,v in MONTH_DICT.items()}
 import pathlib
 PROJECT_DIR = pathlib.Path(__file__).parents[1] #__file__ is an attribute of module variables.py
 DATALOG = PROJECT_DIR.joinpath('data', 'datalog.csv')
+
+#default start date
+from datetime import datetime
+START_DATE = datetime(year=2016, month=1, day=1)
