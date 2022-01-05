1d3222a892694354e5eb02da83d5dadd2cc690eb
Gary Menezes <garymenezes@gary-imac.lan>
2022-01-04 19:42:17 -0800

    added path variables to global variables

--- tools/variables.py
+++ tools/variables.py
@@ -2,3 +2,8 @@
 import calendar
 MONTH_DICT = {idx:month.lower() for idx, month in enumerate(list(calendar.month_name)) if month}
 MONTH_DICT_REV = {v:k for k,v in MONTH_DICT.items()}
+
+#project directory
+import pathlib
+PROJECT_DIR = pathlib.Path(__file__).parents[1] #__file__ is an attribute of module variables.py
+DATALOG = PROJECT_DIR.joinpath('data', 'datalog.csv')
